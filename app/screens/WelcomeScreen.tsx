bb47e97dd2b3b13bedd8e243648cf8c4b617b761
IzuEneh <izueneh21@gmail.com>
2022-10-25 20:19:08 -0600

    starter files

---
+++ app/screens/WelcomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ImageBackground, View, Image, Text, StyleSheet } from 'react-native';
+
+function WelcomeScreen(props: any) {
+    return ;
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default WelcomeScreen;
