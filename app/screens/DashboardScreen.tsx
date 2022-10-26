d52828f725076a2d61665c2f7bc079e35cc6f2a9
IzuEneh <izueneh21@gmail.com>
2022-10-25 20:42:08 -0600

    basic layout of dashboarfd

--- app/screens/DashboardScreen.tsx
+++ app/screens/DashboardScreen.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React from "react";
+import { View, StyleSheet, Text, Platform } from "react-native";
 
 function DashboardScreen(props: any) {
-    return (
-    );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.dateSection}>Today's Date</Text>
+			<View style={styles.weekSection}>
+				<Text>Week day tiles section</Text>
+			</View>
+			<View style={styles.overViewSection}>
+				<Text>Workout Overview section</Text>
+			</View>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+		paddingTop: Platform.OS == "ios" ? 50 : 30,
+		paddingBottom: 0,
+	},
+	dateSection: {
+		height: "10%",
+		backgroundColor: "red",
+	},
+	weekSection: {
+		height: "20%",
+		backgroundColor: "dodgerblue",
+	},
+	overViewSection: {
+		backgroundColor: "grey",
+		flex: 1,
+	},
+});
 
 export default DashboardScreen;
