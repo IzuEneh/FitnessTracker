b323d992d9be681729c5665fca495b64d1863a9a
IzuEneh <izueneh21@gmail.com>
2022-10-25 21:03:41 -0600

    added date

--- app/screens/DashboardScreen.tsx
+++ app/screens/DashboardScreen.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { View, StyleSheet, Text, Platform } from "react-native";
+import { DateTime } from "luxon";
 
 function DashboardScreen(props: any) {
+	const now = DateTime.now();
 	return (
 		<View style={styles.container}>
-			<Text style={styles.dateSection}>Today's Date</Text>
-			<View style={styles.weekSection}>
+			<Text style={[styles.dateSection, styles.defaultSectionVals]}>
+				{now.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)}
+			</Text>
+			<View style={[styles.weekSection, styles.defaultSectionVals]}>
 				<Text>Week day tiles section</Text>
 			</View>
-			<View style={styles.overViewSection}>
+			<View style={[styles.overViewSection, styles.defaultSectionVals]}>
 				<Text>Workout Overview section</Text>
 			</View>
 		</View>
@@ -18,13 +22,14 @@ function DashboardScreen(props: any) {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		padding: 20,
 		paddingTop: Platform.OS == "ios" ? 50 : 30,
-		paddingBottom: 0,
+	},
+	defaultSectionVals: {
+		padding: 20,
 	},
 	dateSection: {
 		height: "10%",
-		backgroundColor: "red",
+		fontSize: 36,
 	},
 	weekSection: {
 		height: "20%",
